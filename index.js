9bb7d36ad72dd9a8953362192316eef5d8febb21
daviinaa <theanthonydavina@gmail.com>
2024-03-07 10:30:51 +0100

    changed ip host

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
- console.log(`App listening at http://localhost:${port}`);
+ console.log(`App listening at http://0.0.0.0:${port}`);
 });
